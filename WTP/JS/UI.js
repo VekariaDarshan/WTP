282ed69e6f31d42fe5b5eae52dc6efd88b45a74f
VekariaDarshan <vekariadarshan2006@gmail.com>
2026-09-09 15:00:39 +0300

    Js- Form

--- WTP/JS/UI.js
+++ WTP/JS/UI.js
@@ -128,7 +128,7 @@ if (inquiryForm) {
             const response = await fetch(GOOGLE_SCRIPT_URL, {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "text/plain;charset=utf-8"
                 },
                 body: JSON.stringify(payload)
             });
