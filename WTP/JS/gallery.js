6d0a7831c0cc30dcf7cbca0df35941604d5a3b97
NimeshSenghani <nimeshsenghani5238@gmail.com>
2026-09-19 14:29:54 +0300

    Modal 6

--- WTP/JS/gallery.js
+++ WTP/JS/gallery.js
@@ -28,8 +28,6 @@ function applyFilter(selectedFilter) {
         return selectedFilter === "all" || item.dataset.category === selectedFilter;
     });
 
-    document.querySelector(".gallery-grid")?.classList.toggle("is-filtered", selectedFilter !== "all");
-
     galleryItems.forEach(item => {
         const matches = selectedFilter === "all" || item.dataset.category === selectedFilter;
         item.classList.toggle("hidden", !matches);
